1806549f07a65ea587684cfe12b066ea3d6eb5ae
SEMANA4 <74093841@continental.edu.pe>
2024-09-16 21:52:12 -0500

    Se modifico orden de rutas

--- routes/web.php
+++ routes/web.php
@@ -23,18 +23,12 @@ Route::get('/', function () {
 
 });
 
-route ::get('celulares', function(){
 
-
-   return"Deko putito & Mikys prra" ;
-});
-
-route ::get('PC', function(){
-    return"Deko el rompe QKS" ;
- });
 
  Route::controller(PacientesController::class)->group(function(){
     route::get('pacientes','index');
     route::get('pacientes/create', 'create');
     route::get('pacientes/{paciente}','show');
  });
+
+
