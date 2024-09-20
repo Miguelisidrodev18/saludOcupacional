2a3b72cc641cf722fe3277ad08ede4e13d67084f
“73760659” <73760659@continental.edu.pe>
2024-09-20 16:51:33 -0500

    Se agrega vistas del informe general

---
+++ resources/views/InformeMedico/Show.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.plantilla')
+
+@section('title', 'InformeMedicoo:' . $InformeMedico)
+
+@section('content')
+    <h1>Información de informes medicos: {{$InformeMedico}}</h1>
+@endsection
---
+++ resources/views/InformeMedico/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.plantilla')
+
+@section('title', 'InformeMedico')
+
+@section('content')
+    <h1>Bievenidos a la pagina de informes medicos</h1>
+@endsection
