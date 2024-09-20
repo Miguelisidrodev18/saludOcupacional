4cd12648140a88b7ed5a78daf6742f2e206de6b9
“73760659” <73760659@continental.edu.pe>
2024-09-20 17:49:40 -0500

    Se corrige vistas index.html de Informe Medico

--- resources/views/InformeMedico/index.blade.php
+++ resources/views/InformeMedico/index.blade.php
@@ -1,7 +1,16 @@
-@extends('layouts.plantilla')
-
-@section('title', 'InformeMedico')
-
-@section('content')
-    <h1>Bievenidos a la pagina de informes medicos</h1>
-@endsection
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenidos a la Página de Informe Clínico</title>
+    <!-- Vincula el archivo CSS externo -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Bienvenidos a la Página de Informe Clínico</h1>
+        <p>Gracias por visitarnos. Aquí puede encontrar toda la información relacionada con sus informes clínicos.</p>
+    </div>
+</body>
+</html>
